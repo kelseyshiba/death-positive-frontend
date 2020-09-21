d44a4419cbf28aaefdd330c6a632102a1aa31f43
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-09-20 19:32:17 -0600

    app setup and action fetch setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
+import { fetchDeaths } from './actions/fetchDeaths';
 
-export default class App extends React.Component {
+class App extends React.Component {
+  
+  componentDidMount (){
+    this.props.fetchDeaths({type: 'GET_DEATHS', 'lalla'})
+  }
+
   render(){
-  return (
-    <div className="App">
-      <h1 className='text-center'>Death Positive +</h1>
-    </div>
-  );
+    return (
+      <div className="App">
+        <h1 className='text-center'>Death Positive +</h1>
+      </div>
+    );
   }
 }
+// const mapStateToProps = state => {
+//   return {
+//     deaths: state.deaths
+//   }
+// }
 
-
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchDeaths: dispatch(fetchDeaths)
+  }
+}
+export default connect(null, mapDispatchToProps)(App)
--- src/actions/fetchDeaths.js
+++ src/actions/fetchDeaths.js
@@ -1,4 +1,6 @@
 export function fetchDeaths(){
-
+    fetch('http://localhost:3000/api/v1/deaths')
+    .then(response => response.json())
+    .then(data => console.log(data))
 }
 
