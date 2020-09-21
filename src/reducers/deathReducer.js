53983a6d17264748efa4e7f4a01dbbcab8763912
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-09-20 20:10:07 -0600

    thunk dispatch setup

--- src/reducers/deathReducer.js
+++ src/reducers/deathReducer.js
@@ -1,5 +1,24 @@
-export default function deathReducer(state = { deaths: [] }, action) {
+export default function deathReducer(state = { deaths: [], loading: false }, action) {
     switch(action.type){
-        
+        case 'LOADING_DEATHS':
+            return {...state, 
+                deaths: [...state.deaths],
+                loading: true}
+        case 'ADD_DEATHS':
+            return state.deaths
+        default: 
+            return state
     } 
 }
+
+// ///  return {
+//     ...state,
+//     cats: [...state.cats],
+//     loading: true
+//   }
+// case 'ADD_CATS':
+//   return {
+//     ...state,
+//     cats: action.cats,
+//     loading: false
+//   }
