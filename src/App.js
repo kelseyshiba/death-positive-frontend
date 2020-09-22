483a6e35ee83b777455ebb2aea138660c02f5377
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-09-22 11:55:46 -0600

    added router, nav, & react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 //import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
 import './App.css';
 import DeathsContainer from './containers/DeathsContainer';
 
@@ -7,11 +9,16 @@ class App extends React.Component {
   
   render(){
     return (
+      <Router>
       <div className="App">
         <h1 className='text-center'>Death Positive +</h1>
         <hr/>
-        <DeathsContainer />
+        <NavBar />
+        <hr/>
+        <Route exact path="/" render={() => <div>Home</div>} />
+        <Route path='/deaths' render={routerProps => <DeathsContainer {...routerProps}/>} />
       </div>
+      </Router>
     );
   }
 }
