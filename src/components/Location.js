0f56b0c61fbf6e47f09894676eafce9f06b229d3
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-09-23 16:27:44 -0600

    added kind and location components to ceremony

---
+++ src/components/Location.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Location = () => {
+    return (
+        <div>
+            <h3>Please enter a Location</h3>
+            <div className='form-group'>
+                <input className='form-control' type='text'/>
+                <input className='form-control' type='text'/>
+                <input className='form-control' type='text'/>
+                <input className='form-control' type='text'/>
+            </div> 
+        </div>
+    )
+}
+
+export default Location;
