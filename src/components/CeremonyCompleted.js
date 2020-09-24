3be66b1d017167c5d208eddd67c612b8182b6100
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-09-23 20:20:05 -0600

    changed ceremony to container

---
+++ src/components/CeremonyCompleted.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CeremonyCompleted = () => {
+    return (
+        <div>Complete!</div>
+    )
+}
+
+export default CeremonyCompleted;
--- src/containers/CeremonyContainer.js
+++ src/containers/CeremonyContainer.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import Kind from './Kind';
-import Location from './Location';
-import Speaker from './Speaker';
+import Kind from '../components/Kind';
+import Location from '../components/Location';
+import Speaker from '../components/Speaker';
+import Narrative from '../components/Narrative';
+import Name from '../components/Name';
+import CeremonyCompleted from '../components/CeremonyCompleted';
 
 class Ceremony extends React.Component {
     state = {
         kind: '',
         pageNum: 0,
-        locationName: '',
+        location_name: '',
         address: '',
         city: '',
         state: '',
-        zip: ''
+        zip: '',
+        speaker: '',
+        name: '',
+        narrative: ''
     }
     //needs state make the component did mount do fetch for the ceremony
     //pass down the ceremony id - to the show route for the ceremony info
     //fetch if created, then make new / form
-    //choices as inputs?
-    //completed stuff or etc..
+   
     selectKind = (event) => {
        
         if(event.target.parentElement.style.backgroundColor === 'lightblue') {
@@ -58,14 +63,43 @@ class Ceremony extends React.Component {
         })
     }
 
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.addCeremony(this.state)
+        this.setState({
+            kind: '',
+            pageNum: 0,
+            locationName: '',
+            address: '',
+            city: '',
+            state: '',
+            zip: '',
+            speaker: '',
+            name: '',
+            narrative: ''
+        })
+    }
+
     render (){
         return (
             <div>
                 {this.state.pageNum === 0 && <Kind selectKind={this.selectKind} changeColor={this.changeColor}
                     reverseChange={this.reverseChange}/>}
                 {this.state.pageNum === 1 && <Location state={this.state} handleChange={this.handleChange}/>}
-                {this.state.pageNum === 2 && <Speaker />}
-                {this.state.pageNum >= 0 &&
+                {this.state.pageNum === 2 && <Speaker state={this.state}  handleChange={this.handleChange}/>}
+                {this.state.pageNum === 3 && <Narrative state={this.state} handleChange={this.handleChange}/>}
+                {this.state.pageNum === 4 && <Name state={this.state} handleChange={this.handleChange}/>}
+                {this.state.pageNum === 5 && <CeremonyCompleted />}
+                
+                {this.state.pageNum === 0 && 
+                   <div className='row'>
+                       <div className='col-sm-6'>
+                        </div>
+                        <div className='col-sm-6'>
+                           <button onClick={this.handleNext} className='btn btn-primary'>Next</button>
+                        </div>
+                    </div>}
+                {this.state.pageNum > 0 && this.state.pageNum < 4 &&
                 <div className='row'>
                     <div className='col-sm-6'>
                         <button onClick={this.handleBack} className='btn btn-primary'>Back</button>
@@ -75,6 +109,10 @@ class Ceremony extends React.Component {
                     </div>
                 </div>
                 }
+                {this.state.pageNum === 4 && 
+                <div>
+                    <button className='btn btn-success' onClick={(event) => this.handleSubmit(event)}>Submit</button>
+                </div>}
             </div>
         )
     }
