381511a79eaff4ce30abf65e7d3ff442fd4ec5ea
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-09-26 15:07:41 -0600

    removed loading temporarily

--- src/actions/fetchDeaths.js
+++ src/actions/fetchDeaths.js
@@ -1,10 +1,11 @@
 export const fetchDeaths = () => {
     return (dispatch) => {
-        dispatch({type: 'LOADING_DEATHS'})
+    
+        // dispatch({type: 'LOADING_DEATHS'})
         fetch('http://localhost:3000/api/v1/deaths')
         .then(response => response.json())
-        .then(resp => {
-            dispatch({type: 'FETCH_DEATHS', deaths: resp.data})
+        .then(data => {
+            dispatch({type: 'FETCH_DEATHS', payload: data})
         })
     }
 }
