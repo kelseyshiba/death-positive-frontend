05295f4c9f0b52a0966d06d7a77bbc1c734c857e
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-09-27 22:07:26 -0600

    death card styling

--- src/components/deaths/Deaths.js
+++ src/components/deaths/Deaths.js
@@ -7,14 +7,14 @@ class Deaths extends React.Component {
     render (){
         console.log('deaths', this.props.deaths)
         return (
-            <div class="card-deck">
+            <div className="card-deck">
                 {this.props.deaths && this.props.deaths.map(death =>
-                <div class="card" key={death.id}>
+                <div className="card" key={death.id}>
                 <img height='250px' src={process.env.PUBLIC_URL + '/images/gravestone.png'} alt='card-bkgnd'/><br></br>
-                <div class="card-body">
-                  <h5 class="card-title">Rest in Peace</h5>
-                  <button class="btn btn-secondary"><Link to={`/deaths/${death.id}`}>{death.attributes.person}</Link></button>
-                <p class="card-text"><small class="text-muted">{death.attributes.date}</small></p>
+                <div className="card-body">
+                  <h5 className="card-title">Rest in Peace</h5>
+                  <button className="btn btn-secondary"><Link to={`/deaths/${death.id}`}>{death.attributes.person}</Link></button>
+                <p className="card-text"><small className="text-muted">{death.attributes.date}</small></p>
                 </div>
                 </div>
                 )}
